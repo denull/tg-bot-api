3f98edc4d0bd73b1db049a4545a1b1bafe71301b
Denis Olshin <me@denull.ru>
2024-10-25 14:00:46 +0300

    removing null and undefined fields from params

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ function streamToFile(stream, name, type) {
   });
 }
 
+function isFileLike(obj) {
+  return obj instanceof Buffer || obj instanceof ArrayBuffer || isTypedArray(obj) || obj instanceof DataView ||
+    obj instanceof Readable || obj instanceof Blob || obj instanceof File;
+}
+
+function filterEmptyValues(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(filterEmptyValues);
+  }
+  if (typeof obj != 'object' || obj === null || isFileLike(obj)) {
+    return obj;
+  }
+  return Object.fromEntries(Object
+    .keys(obj)
+    .filter(key => obj[key] !== null && obj[key] !== undefined)
+    .map(key => [key, filterEmptyValues(obj[key])])
+  );
+}
+
 class TelegramError extends Error {
   constructor(request, { error, cause }) {
     super(error ? error.description : cause.message, cause && { cause });
@@ -44,46 +63,46 @@ class TelegramBotAPI {
   async callMethod(method, params = {}, retryAttempts = 10) {
     let attempt = 0;
     try {
+      let formData = null;
+      const fields = filterEmptyValues(params || {});
+      for (let k in fields) {
+        if (isFileLike(fields[k])) {
+          formData = new FormData();
+        }
+      }
+
+      if (formData) {
+        for (let k in fields) {
+          let meta = {};
+          for (const field of ['name', 'type']) {
+            if (`${k}$${field}` in fields) {
+              meta[field] = fields[`${k}$${field}`];
+              formData.delete(`${k}$${field}`); // In case we already added it
+            }
+          }
+          const value = fields[k];
+          if ((value instanceof Blob || value instanceof File) && !meta.name && !meta.type) {
+            formData.set(k, value); // No need to transform
+          } else
+          if (value instanceof Blob || value instanceof File || value instanceof Buffer || value instanceof ArrayBuffer ||
+            isTypedArray(value) || value instanceof DataView) {
+            formData.set(k, new File([value], meta.name || value.name, { type: meta.type || value.type }));
+          } else
+          if (value instanceof Readable) {
+            formData.set(k, await streamToFile(value, meta.name, meta.type));
+          } else
+          if (typeof value === 'object') {
+            formData.set(k, JSON.stringify(value));
+          } else {
+            formData.set(k, value);
+          }
+        }
+      }
+
       while (true) { // Handle multiple attempts without recursion
-        let formData = null;
-        for (let k in params) {
-          if (params[k] instanceof Buffer || params[k] instanceof ArrayBuffer || isTypedArray(params[k]) || params[k] instanceof DataView ||
-            params[k] instanceof Readable || params[k] instanceof Blob || params[k] instanceof File) {
-            formData = new FormData();
-          }
-        }
-
-        if (formData) {
-          for (let k in params) {
-            let meta = {};
-            for (const field of ['name', 'type']) {
-              if (`${k}$${field}` in params) {
-                meta[field] = params[`${k}$${field}`];
-                formData.delete(`${k}$${field}`); // In case we already added it
-              }
-            }
-            const value = params[k];
-            if ((value instanceof Blob || value instanceof File) && !meta.name && !meta.type) {
-              formData.set(k, value); // No need to transform
-            } else
-            if (value instanceof Blob || value instanceof File || value instanceof Buffer || value instanceof ArrayBuffer ||
-              isTypedArray(value) || value instanceof DataView) {
-              formData.set(k, new File([value], meta.name || value.name, { type: meta.type || value.type }));
-            } else
-            if (value instanceof Readable) {
-              formData.set(k, await streamToFile(value, meta.name, meta.type));
-            } else
-            if (typeof value === 'object') {
-              formData.set(k, JSON.stringify(value));
-            } else {
-              formData.set(k, value);
-            }
-          }
-        }
-
         const response = await fetch(this.endpoint.replaceAll(/{token}/gi, this.token).replaceAll(/{method}/gi, method), {
           method: 'post',
-          body: formData || JSON.stringify(params),
+          body: formData || JSON.stringify(fields),
           ...(!formData && {
             headers: {
               'Content-Type': 'application/json',
